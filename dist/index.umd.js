a6cfdfcd4209ca8e7e7d277ba8a5ca1a45a30c09
Bjørn Egil <bjorn.egil.hansen@gmail.com>
2019-03-01 15:25:26 +0100

    Added ts, tslint, perttier

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -1,2 +1,2 @@
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?n(exports):"function"==typeof define&&define.amd?define(["exports"],n):n(e.y={})}(this,function(e){e.MyClass=function(){}});
+!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?n(exports):"function"==typeof define&&define.amd?define(["exports"],n):n(e.reduxTransactional={})}(this,function(e){e.MyClass=function(){}});
 //# sourceMappingURL=index.umd.js.map
